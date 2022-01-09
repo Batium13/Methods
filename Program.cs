2d944d6d1420fbdac5988afba40729ee18343446
Batium <batuhan.altinel@hotmail.com>
2022-01-09 10:28:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // erişim_Belirtci  geriDönüş_tipi  methodAdı(ParametrelerListesi / arguman)
+            //{
+                // komutlar;
+                //return;
+            //}
+
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a+b);
+            
+            int sonuc = Topla(a,b);
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttirveTopla(ref a,ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+           
+        }
+        static int Topla(int deger1,int deger2){
+        
+            return deger1+deger2;
+
+        }
+    }
+
+    class Metotlar
+    {
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirveTopla(ref int deger1,ref int deger2)
+        {
+            deger1 +=1;
+            deger2 +=1;
+
+            return deger1+deger2;
+        }
+    }
+}
